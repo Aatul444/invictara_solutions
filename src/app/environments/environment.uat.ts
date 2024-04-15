9b2f140cf1e7b7acf19b39b10e30fef39bfe63b7
Atul Chaudhary <a.atulchaudhary444@gmail.com>
2024-04-15 11:46:10 +0530

    l

---
+++ src/app/environments/environment.uat.ts
@@ -0,0 +1,19 @@
+
+
+export const environment = {
+    env: "UAT",
+    production: false,
+    appUrl:"https://matesonrent.com.au",
+    firebaseConfig:{
+      apiKey: "AIzaSyA3QJ8CJQLZ3pE76niI95wriYeFi3ACHmg",
+  authDomain: "invictara-web.firebaseapp.com",
+  projectId: "invictara-web",
+  storageBucket: "invictara-web.appspot.com",
+  messagingSenderId: "899928524548",
+  appId: "1:899928524548:web:7a3a944abd70d7ae7e3251",
+  measurementId: "G-0TLLXTD4QE"
+    }
+  };
+  
+
+  
